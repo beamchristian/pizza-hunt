df1a8fb81805ceb67b6ed3ce459fa133a229948e
beamchristian <beamchristian@yahoo.com>
2021-12-14 12:59:29 -0500

    routes and front end interaction

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -14,7 +14,7 @@ const commentController = {
       })
       .then(dbPizzaData => {
         if (!dbPizzaData) {
-          res.status(404).json({ messsage: 'No pizza found with this id!' });
+          res.status(404).json({ message: 'No pizza found with this id!' });
           return;
         }
         res.json(dbPizzaData);
